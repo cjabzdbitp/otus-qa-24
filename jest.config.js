f88adabeeb76e71037fa7097211783aec0bbf3a6
Safiya Panushkina <s.panushkina@3commas.io>
2025-01-05 16:07:44 +0000

    add report

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,19 @@ const config = {
   testEnvironment: 'node',
   testMatch: ['**/specs/**/*.spec.js'],
   coverageProvider: 'v8',
-  reporters: [['github-actions', { silent: false }], 'summary']
+  reporters: [
+    ['github-actions', { silent: false }],
+    'summary',
+    [
+      'jest-html-reporter',
+      {
+        pageTitle: 'Test Report',
+        outputPath: './reports/test-report.html',
+        includeFailureMsg: true,
+        includeSuiteFailure: true
+      }
+    ]
+  ]
 };
 
 export default config;
