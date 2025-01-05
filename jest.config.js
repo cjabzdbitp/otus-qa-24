b8eb21f6ac335e78ec8ae14cacea14157342a391
Safiya Panushkina <s.panushkina@3commas.io>
2025-01-05 17:07:28 +0000

    update configs

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,22 @@
 const config = {
-  testEnvironment: 'node',
   testMatch: ['**/specs/**/*.spec.js'],
   coverageProvider: 'v8',
   reporters: [
+    'default',
     ['github-actions', { silent: false }],
     'summary',
     [
-      'jest-html-reporter',
+      'jest-html-reporters',
       {
-        pageTitle: 'Test Report',
-        outputPath: './reports/test-report.html',
-        includeFailureMsg: true,
-        includeSuiteFailure: true
+        publicPath: './reports/html-report',
+        filename: 'index.html'
       }
     ]
-  ]
+  ],
+  testEnvironment: 'allure-jest/node',
+  testEnvironmentOptions: {
+    resultsDir: 'reports/allure-results'
+  }
 };
 
 export default config;
